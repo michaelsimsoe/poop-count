1b5774d150cb288131d0db6f7658e5a93fe54f27
michaelsimsoe <mikke.korgen@gmail.com>
2019-11-23 00:34:52 +0100

    change time zone

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,6 @@ module PoopCount
 
     # Don't generate system test files.
     config.generators.system_tests = nil
-    config.time_zone = "Helsinki"
+    config.time_zone = "Stockholm"
   end
 end
